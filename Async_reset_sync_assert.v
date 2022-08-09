0d2ebe5f8b60a5df1ef5a84832945f06f23fbddc
frank <lynnioi0405@gmail.com>
2022-08-09 08:39:48 +0800

    changed brief info

--- Async_reset_sync_assert.v
+++ Async_reset_sync_assert.v
@@ -1,23 +1,22 @@
-//////////////////////////////////////////////////////////////////////////////////////
-//Version:v1.0.0
-//Date:2018-7-12 19:58:47
-//Author:Forrest
-//Email:lynnioi@163.com/lynnioi0405@gmail.com
-//QQ:121392215
-//Location:Beijing
-//http://www.4fpga.cn  
-//http://www.4forrest.com/
+/*------------------------------------------------------------------------------------
+Version:v1.0.1
+Date:2018-7-12 19:58:47
+Author:Frank
+Email:fan30b79@gmail.com/lynnioi@163.com
+Wechat:121392215
+Location:Beijing
 
-//Description: Async reset Sync assert
-//1) _i PIN input  
-//2) _o PIN output
-//3) _n PIN active low
-//4) _dg debug signal 
-//5) _r  reg delay
-//6) _s state machine
-//////////////////////////////////////////////////////////////////////////////////////
+
+Description: Async reset Sync assert
+1) _i PIN input  
+2) _o PIN output
+3) _n PIN active low
+4) _dg debug signal 
+5) _r  reg delay
+6) _s state machine
+-------------------------------------------------------------------------------------*/
 wire rst_n;
-wire sys_clk_100m;
+wire sys_clk;
 wire locked;
 wire sys_rst_n;
 wire async_RESET_n;
@@ -26,7 +25,7 @@ reg sys_rst_n_r0,sys_rst_n_r;
 assign 	   sys_rst_n = sys_rst_n_r		;
 assign async_RESET_n = rst_n | locked	;
 
-always @(posedge sys_clk_100m or negedge async_RESET_n)
+always @(posedge sys_clk or negedge async_RESET_n)
 begin
 	if(!async_RESET_n)
 		begin
